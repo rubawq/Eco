014ec14526e74c6883a827c76b9ce4e625119679
rubawq <Rubawq1@gmail.com>
2025-05-07 11:51:50 +0300

    Feat: card list page

---
+++ src/components/cardList.tsx
@@ -0,0 +1,44 @@
+// import React from "react";
+// import CardCom from "./cardComponent";
+
+const products = [{
+    title: 'T-shirt',
+    description: 'white T-shirt',
+    price: 10,
+    image: 'public/pic/pi1.jpg'
+
+},
+{
+    title: 'Wallet',
+    description: 'A fancy wallet',
+    price: 20,
+    image: 'public/pic/pi1.jpg'
+},
+
+{
+    title: 'Cup',
+    description: 'A tea cup',
+    price: 5,
+    image: 'public/pic/pi1.jpg'
+}, {
+    title: 'T-shirt',
+    description: 'white T-shirt',
+    price: 10,
+    image: 'public/pic/pi1.jpg'
+},
+{
+    title: 'Wallet',
+    description: 'A fancy wallet',
+    price: 20,
+    image: 'public/pic/pi1.jpg'
+},
+
+{
+    title: 'Cup',
+    description: 'A tea cup',
+    price: 5,
+    image: 'public/pic/pi1.jpg'
+},
+
+];
+export default products;
---
+++ src/components/typeCom.ts
@@ -0,0 +1,8 @@
+//card Types 
+export interface CardType {
+    title: string;
+    description : string;
+    price: number
+    image: string;
+
+}
